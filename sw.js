e71f9d18bf8aa27fb7df0ecfc1d09479b9e7625b
serjfri <malquebien@gmail.com>
2025-09-14 17:27:03 +0200

    Corregir rutas de build para gh-pages

--- sw.js
+++ sw.js
@@ -1,7 +1,7 @@
 const CACHE_NAME = 'mis-acordes-v1';
 const urlsToCache = [
-  '/',
-  '/manifest.json'
+  '/acordes/',
+  '/acordes/public/icons/site.webmanifest'
 ];
 
 self.addEventListener('install', (event) => {
